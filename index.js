2b8d616374e807be87173f8569425c1c963d3b44
raisyuna <raisyuna@gmail.com>
2024-03-07 00:49:01 +0600

    first

---
+++ index.js
@@ -0,0 +1,26 @@
+//task1
+let smile = ':)'
+for (let i = 0; i < 10; i++) {
+    console.log(smile);
+    smile += ':)'
+ }
+
+ //task3
+ for (let i = 70; i >= 40; i--) {
+if (i % 2 == 0) {
+    console.log(i)
+}
+}
+
+// task4
+let sum = 0
+for (let i = 1; i <= 100; i++) {
+    sum += i
+}
+console.log(sum);
+
+// task3
+// let start = 1000
+// for (let i = 1000; 50 < i ; i / 2) {
+//     console.log(i);
+// }
